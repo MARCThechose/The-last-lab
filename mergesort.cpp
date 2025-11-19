00a7cf0bc61961f787ec748792637a4394e05704
MARCThechose <iamapersonintheinternet@gmail.com>
2025-11-19 19:38:44 +0800

    mergesort typa thing

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,59 +1,62 @@
 #include <iostream>
 #include <cmath>
 
-int* merge(int * array,int size)
+int* merge_sort( int * array, int size);
+void merge(int * result, int* left, int Lsize, int* right, int right_size);
+
+
+int main()
 {
-    // TODO: implement a base case that automatically returns data
-    int length_array = size; 
+    int data[] = {24, 23, 45543, 3215};
+    int* sorted_data = merge_sort(data, 4);
+    
+    for (int i = 0; i < 4; i++)
+    {
+        std::cout << sorted_data[i] << " ";
+    }
+
+}
+int* merge_sort(int * array,int size)
+{
+    // TODO: implement a base case that automatically returns data 
     // sizeof(array)/sizeof(array[0]);
    // calculate data length
-    if (length_array < 2) return array;
+    if (size < 2) return array;
     
-    int midpoint = floor(length_array / 2);
+    int midpoint = floor(size / 2);
    
-    int*left_half = new int[midpoint];
-    int*right_half = new int[length_array - midpoint];
+    int* left_half = new int[midpoint];
+    int* right_half = new int[size - midpoint];
 
     for (int i = 0; i < midpoint; i++) 
     {
         left_half[i] = array[i];
     
     }
-    for (int i = midpoint; i < length_array; i++) 
+    for (int i = midpoint; i < size; i++) 
     {
         right_half[i - midpoint] = array[i];
     }
 
-    left_half = merge(left_half, midpoint);
-    right_half = merge(right_half, midpoint);
+    left_half = merge_sort(left_half, midpoint);
+    right_half = merge_sort(right_half, size - midpoint);
     
-    //merge(left_half, right_half, data)
-    for (int i = 0; i < length_array; i++) 
-    {
-        
-        if (left_half[0] < left_half[0])
-        {
-            array[i] = left_half[0];
-            
-        }
-        else
-        {
-            array[i] = right_half[0];
-        }
-    }
-    
+    merge (array, left_half, midpoint, right_half, size - midpoint);
+
+    delete[] left_half;
+    delete[] right_half;
 
     return array;
 }
 
-int main()
+void merge(int * result, int* left, int Lsize, int* right, int Rsize)
 {
-    int data[] = {24, 23, 45543, 3215};
-    int* sorted_data = merge(data, 4);
+    int i = 0; int j = 0; int k = 0;
 
-    for (int i = 0; i < 4; i++)
-    {
-        std::cout << sorted_data[i];
-    }
 
+    while ( i < Lsize && j < Rsize) result[k++] = (left[i] <= right[j]) ? left[i++] : right[j++];
+
+    while (i < Lsize) result[k++] = left [i++];
+
+    while (j < Rsize) result[k++] = right [j++];
 }
